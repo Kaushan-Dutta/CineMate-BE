bb17142772603d4f245b106ea3f8dc29e2ff8c60
Kaushan <kaushandutta5@gmail.com>
2023-10-29 23:17:17 +0530

    updating package

--- server.js
+++ server.js
@@ -18,9 +18,11 @@ app.use('/user', require('./Routers/useroute'));
 
 
 app.get("/test-mode", (req, res) => {
-  res.status(200).json({ message: "Welcome to text-mode" });
+  res.status(200).json({ message: "Welcome to test-mode" });
 });
-
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to Cine Mate" });
+});
 const NODE_PORT = process.env.PORT || 5000;
 
 server.listen(NODE_PORT, () => console.log("Server is running on port ", NODE_PORT));
